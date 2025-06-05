88ab9f4d341cbf554be5664808079c5491c3056a
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-06-05 18:30:22 -0300

    configures tailwindcss and implements default page.

---
+++ src/MainPage.tsx
@@ -0,0 +1,18 @@
+import PlayingCards from "/public/playing_cards.svg";
+
+export default function MainPage() {
+  return (
+    <main className="text-stone-400">
+      <h1 className="font-bold">Ankit</h1>
+      <p className="text-[1.2rem]">
+        Termine uma sessão de estudos ou clique em{" "}
+        <img
+          className="w-[30px] h-[30px] inline"
+          src={PlayingCards}
+          alt="Cartões de baralho contornados de branco"
+        />{" "}
+        para adicionar seus cartões no Anki!
+      </p>
+    </main>
+  );
+}
