e8f4c5e4529a110ba111226018baf3874dab71fa
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-06-10 10:20:14 -0300

    adds status to verify if the user is connected to AnkiConnect.

--- src/components/FormPage.tsx
+++ src/components/FormPage.tsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react";
+
 export default function FormPage() {
-  return <h1>This is the form page!</h1>;
+  const [connectionStatus, setConnectionStatus] = useState<
+    "loading" | "error" | "success"
+  >("loading");
+
+  useEffect(() => {
+    fetch("http://localhost:8765").catch((_error) =>
+      setConnectionStatus("error")
+    );
+  }, [connectionStatus]);
+
+  return (
+    <main className="border-white">
+      {connectionStatus === "loading" && <h1>Carregando</h1>}
+      {connectionStatus === "error" && <h1>ERRO!!!!!</h1>}
+      {connectionStatus === "success" && <h1>FORMULÁRIO AQUI!!!</h1>}
+    </main>
+  );
 }
