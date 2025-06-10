d5b434d4b7754af19c42ea49604d8194b17d9fcb
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-06-10 16:01:52 -0300

    handles connection to Ankit errors.

--- src/components/FormPage.tsx
+++ src/components/FormPage.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import signalDisconnectedIcon from "/public/signal_disconnected.svg";
 
 export default function FormPage() {
-  const [connectionStatus, _setConnectionStatus] = useState<
+  const [connectionStatus, setConnectionStatus] = useState<
     "loading" | "error" | "success"
   >("error");
 
-  // useEffect(() => {
-  //   fetch("http://localhost:8765").catch((_error) =>
-  //     setConnectionStatus("error")
-  //   );
-  // }, [connectionStatus]);
+  useEffect(() => {
+    const fetchDecksPayload = {
+      action: "deckNames",
+      version: 6,
+    };
+
+    fetch("http://localhost:8765", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fetchDecksPayload),
+    })
+      .then((response) => response.json())
+      .then((_json) => {
+        setConnectionStatus("success");
+      })
+      .catch((_error) => setConnectionStatus("error"));
+  }, [connectionStatus]);
 
   return (
     <main className="border-white h-screen justify-center items-center gap-3">
@@ -31,13 +45,16 @@ export default function FormPage() {
                   Verifique se o add-on "AnkiConnect" foi adicionado ao seu Anki
                   ou se o Anki está aberto.
                 </p>
-                <button className="rounded-md py-2 px-3 flex justify-center items-center bg-blue-700 hover:bg-blue-800 cursor-pointer">
+                <button
+                  onClick={() => setConnectionStatus("loading")}
+                  className="rounded-md py-2 px-3 flex justify-center items-center bg-blue-700 hover:bg-blue-800 cursor-pointer"
+                >
                   Tentar novamente
                 </button>
               </div>
             </>
           ) : (
-            <h2 className="font-bold">Carregando...</h2>
+            <h2 className="font-bold text-stone-400">Carregando...</h2>
           )}
         </>
       )}
