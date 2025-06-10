3847c8cf1cc4941cfdd076c554bd18355f7ebe6f
Gentis <fernandes.gentilkadsonjac@gmail.com>
2025-06-09 22:05:01 -0300

    vite now builds both background and contentScript files separately.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        background: resolve(__dirname, "src/background.ts"),
+        content: resolve(__dirname, "src/contentScript.ts"),
+      },
+      output: {
+        entryFileNames: "assets/[name].js",
+      },
+    },
+  },
 });
