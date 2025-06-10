4feb7c81f0991250e55a36b0b736cd2f1926cefc
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2025-06-10 09:44:58 -0300

    splits the main page and form page into two separate components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import MainPage from "./MainPage";
+import type { Flashcard } from "./types";
+import { useEffect, useState } from "react";
+import MainPage from "./components/MainPage";
+import FormPage from "./components/FormPage";
 
 function App() {
-  return <MainPage />;
+  const [flashcards, setFlashcards] = useState<Flashcard[]>([]);
+
+  useEffect(() => {
+    chrome.runtime.sendMessage(
+      { type: "REQUEST_FLASHCARDS_FROM_BACKGROUND" },
+      (response) => {
+        if (response && response.flashcards) setFlashcards(response.flashcards);
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(flashcards);
+  }, [flashcards]);
+  return <>{flashcards.length === 0 ? <MainPage /> : <FormPage />}</>;
 }
 
 export default App;
---
+++ src/components/FormPage.tsx
@@ -0,0 +1,3 @@
+export default function FormPage() {
+  return <h1>This is the form page!</h1>;
+}
