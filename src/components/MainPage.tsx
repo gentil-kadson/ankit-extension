0a5e6e446ebacd7f725fdbace528dc62b586f0ea
Gentis <fernandes.gentilkadsonjac@gmail.com>
2025-06-13 17:49:30 -0300

    restyles h1 tag's font size; adds Ankit icon to main page title; make paragraph's font size in the main page smaller.

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,17 +1,25 @@
 import PlayingCards from "/public/playing_cards.svg";
+import AnkitIcon from "/public/ankit_icon.svg";
 
 export default function MainPage() {
   return (
     <main className="text-stone-400 border-none">
-      <h1 className="font-bold">Ankit</h1>
-      <p className="text-[1.2rem]">
-        Termine uma sessão de estudos ou clique em{" "}
+      <div className="flex items-center justify-center gap-1">
+        <img
+          className="w-[35px] h-[35px]"
+          src={AnkitIcon}
+          alt="Caixa de pacotes selada e contornada de cinza."
+        />
+        <h1 className="font-bold">Ankit</h1>
+      </div>
+      <p>
+        Clique em{" "}
         <img
           className="w-[30px] h-[30px] inline"
           src={PlayingCards}
           alt="Cartões de baralho contornados de branco"
         />{" "}
-        para adicionar seus cartões no Anki!
+        em uma sessão de estudos finalizada para adicionar seus cartões no Anki!
       </p>
     </main>
   );
